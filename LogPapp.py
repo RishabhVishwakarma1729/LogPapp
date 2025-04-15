d6be82ff4717ced1e11e545737012548752d3f38
RISHABH VISHWAKARMA <166126955+RishabhVishwakarma1729@users.noreply.github.com>
2025-04-15 16:28:23 +0530

    Update LogPapp.py

--- LogPapp.py
+++ LogPapp.py
@@ -74,20 +74,48 @@ st.set_page_config(page_title="Caco-2 Permeability Predictor", layout="centered"
 st.title("🧪 Caco-2 Permeability Prediction")
 st.markdown("Enter SMILES strings to predict **Caco-2 permeability**.")
 st.markdown("""
-**Caco-2 permeability** refers to the ability of a compound to pass through a layer of Caco-2 cells.
+
+**Caco-2 permeability** refers to the ability of a compound to pass through a layer of Caco-2 cells, which are human colorectal adenocarcinoma cells commonly used as an *in vitro* model of the intestinal barrier.
 
 ---
 
 ### 🔍 Key Points
 
-- **Caco-2 Cells**: Simulate the human intestinal barrier
-- **Measured As**: `P_app` (Apparent Permeability), in cm/s or log(P_app)
-- **Why It Matters**: Predicts **oral bioavailability**, reduces early animal testing
+- **Caco-2 Cells**: Form tight junctions after ~21 days, resembling the intestinal lining.
+- **Purpose**: Simulates **passive diffusion** across the intestinal wall.
+- **Measured As**:  
+  - `P_app` (Apparent Permeability Coefficient), in cm/s or log(P_app)
 
 ---
+
+### 🧬 Why It Matters
+
+- Predicts **oral bioavailability**
+- Flags **poorly absorbed** or **efflux-prone** compounds
+- Reduces reliance on early animal testing
+
+
 """)
 
 smiles_input = st.text_area("Enter SMILES (one per line):", height=150)
+
+st.markdown("---")
+st.markdown("### 📊 Our Model's Performance")
+st.markdown("Split used - Scaffold")
+st.markdown("""
+**Training Set**  
+- RMSE: `0.1027`  
+- R²: `0.9834`  
+
+**Validation Set**  
+- RMSE: `0.1626`  
+- R²: `0.9559`  
+
+**Test Set**  
+- RMSE: `0.1347`  
+- R²: `0.9712`
+""")
+
 
 if st.button("Predict"):
     smiles_list = [s.strip() for s in smiles_input.strip().split("\n") if s.strip()]
